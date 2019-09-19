4e9425083c6f6c6786395961044ec17a6ebd7688
Jack Slingerland <jack.slingerland@cengage.com>
2019-09-19 09:54:46 -0400

    Changing how feature flag caching works

--- README.md
+++ README.md
@@ -17,13 +17,13 @@ The best way to install the library is with Composer.
     // The feature flag product key.  This can be found in the Kernl web app
     // in the "Feature Flags" menu.
     $kernlFeatureFlagProductKey = '58cb023bc9689c1fe811615d';
-    
+
     // The user identifier is how Kernl identifies the user requesting flags.
     // This should be unique for every user.
     $userIdentifier = 'jack@kernl.us';
-    
+
     $kff = new kernl\WPFeatureFlags($kernlFeatureFlagProductKey, $userIdentifier);
-    
+
     // This says "For the product defined above, does this flag exists,
     // and if so, is it active for the given user?".
     if ($kff->active('GITHUB_ON_OFF')) {
@@ -40,4 +40,4 @@ The best way to install the library is with Composer.
 
 ### Performance Optimizations
 
-This library makes some small performance optimizations by storing flag data for users as transients.  The time length is 5 minutes, so your flag state will become eventually consistent.  Storing the data as transients allows it to be cached by commmon WordPress caching plugins.
+This library makes some small performance optimizations by storing flag data for users as transients.  The default time length is 5 minutes, so your flag state will become eventually consistent.  Storing the data as transients allows it to be cached by commmon WordPress caching plugins.
--- src/WPFeatureFlags.php
+++ src/WPFeatureFlags.php
@@ -4,90 +4,51 @@ namespace kernl;
 
 class WPFeatureFlags {
 
+  private $cacheTimeMinutes;
   private $failActive;
-  private $flagCount = 0;
-  private $flags = [];
   private $productKey;
-  private $salt = '1';
   private $userIdentifier;
 
-  public function __construct($productKey, $userIdentifier, $failActive = false) {
+  public function __construct($productKey, $userIdentifier, $failActive = false, $cacheTimeMinutes = 5) {
+      $this->cacheTimeMinutes = $cacheTimeMinutes;
       $this->productKey = $productKey;
       $this->userIdentifier = $userIdentifier;
       $this->failActive = $failActive;
-      $this->flags = get_transient($this->getFlagKey());
-      $this->flagCount = count($this->flags);
   }
 
   public function active($flagIdentifier) {
-
-      // Does not have flags or key doesn't exist.
-      if (!$this->hasFlags() || !$this->flagExists($flagIdentifier)) {
-          $lastChecked = get_transient($this->getLastCheckKey());
-
-          // No last checked transient or it's been more than 5 minutes since last check
-          if (!$lastChecked || ((time() - $lastChecked) > 60 * 5)) {
-              $this->updateFlags();
-
-              // Flags updated, lets see if we have anything new.
-              if (!$this->hasFlags() || !$this->flagExists($flagIdentifier)) {
-                  return $this->failActive;
-              } else {
-                  return $this->flags[$flagIdentifier];
-              }
-
-          } else {
-              return $this->failActive;
-          }
-
-      // Flag found, return.
-      } else {
-          return $this->flags[$flagIdentifier];
+      $hash = md5("{$this->productKey}-{$this->userIdentifier}");
+      $flagKey = "kff-${hash}";
+      $flags = get_transient($flagKey);
+      if (!$flags || count($flags) == 0) {
+        $this->updateFlags($flagKey);
+        $flags = get_transient($flagKey);
       }
+      if (!$flags || count($flags) == 0 || !isset($flags[$flagIdentifier])) {
+        return $this->failActive;
+      } else {
+        return $flags[$flagIdentifier];
+      }
+
   }
 
-  private function getFlagKey() {
-      $hash = md5("{$this->productKey}-{$this->userIdentifier}-{$this->salt}");
-      return "kernlff-{$hash}";
-  }
-
-  private function getLastCheckKey() {
-      $hash = md5("{$this->productKey}-{$this->userIdentifier}-{$this->salt}");
-      return "kernlchk-{$hash}";
-  }
-
-  private function flagExists($flagIdentifier) {
-      return isset($this->flags[$flagIdentifier]);
-  }
-
-  private function hasFlags() {
-      return $this->flagCount > 0;
-  }
-
-  private function updateFlags() {
+  private function updateFlags($flagKey) {
       $headers = array('Content-Type' => 'application/json');
       $url = "https://kernl.us/api/v2/public/feature-flags/$this->productKey?identifier=$this->userIdentifier";
       $response = wp_remote_get($url);
       if (is_array($response) && $response['response']['code'] === 200) {
           try {
               $decodedFlags = json_decode($response['body']);
+              $flags = array();
               foreach ($decodedFlags as $flag) {
-                  $this->flags[$flag->flag] = $flag->active;
-                  $this->flagCount++;
+                  $flags[$flag->flag] = $flag->active;
               }
-
-              // Update last checked.
-              set_transient($this->getLastCheckKey(), time(), 60 * 5);
-
-              // Update flags transient
-              set_transient($this->getFlagKey(), $this->flags, 60 * 60 * 24);
-
+              set_transient($flagKey, $flags, 60 * $this->cacheTimeMinutes);
           } catch(Exception $err) {
-              // echo " There was an error de-serializing the feature flag request body.";
+            //   echo " There was an error de-serializing the feature flag request body.";
           }
-
       } else {
-          // echo "There was an error in your feature flag request.";
+        //   echo "There was an error in your feature flag request.";
       }
   }
 }
